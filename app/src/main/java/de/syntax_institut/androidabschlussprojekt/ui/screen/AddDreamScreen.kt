ccaf01ba2fd30a9bd87cf113f1af1cb59a23c6dd
Anh Thy Vo <anhthyvo@icloud.com>
2025-05-09 12:41:30 +0200

    Ordnerstruktur aufgebaut

---
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/screen/AddDreamScreen.kt
@@ -0,0 +1,19 @@
+package de.syntax_institut.androidabschlussprojekt.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AddDreamScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AddDreamScreenPreview() {
+    // Use Theme here
+    AddDreamScreen()
+}
---
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/screen/HomeScreen.kt
@@ -0,0 +1,19 @@
+package de.syntax_institut.androidabschlussprojekt.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    // Use Theme here
+    HomeScreen()
+}
